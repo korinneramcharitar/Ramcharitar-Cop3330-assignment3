b6d442e6d899d925d2b2cdaddf42fb3f0fbadb58
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-20 18:40:23 -0400

    Add files via upload

---
+++ src/main/java/ex44/App.java
@@ -0,0 +1,38 @@
+package ex44;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class App {
+    String name;
+    int price;
+    int quantity;
+
+
+    public void userInput(){
+    //Create Strings from JSON file
+
+        //Have prompt for user to input Product name
+
+        //Have prompt if input does not match strings
+
+
+        //continue on to print price and quantity available if string .equals input
+
+    }
+public void main(String[] args){
+        //read in JSON file as strings
+
+
+    //attach strings to product, price, and quantity
+
+    //prompt userInput
+
+    //printlines
+
+
+}
+    }
+
