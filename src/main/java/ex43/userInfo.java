7c2d4c1f91117cb3922c9a0ecc57f9c0cc27d384
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-20 18:25:35 -0400

    Add files via upload

---
+++ src/main/java/ex43/userInfo.java
@@ -0,0 +1,53 @@
+package ex43;
+
+
+import java.io.File;
+import java.util.Scanner;
+
+public class userInfo {
+
+    public static void main(String[] args) {
+
+//Input
+        Scanner newScan = new Scanner(System.in);
+
+        String siteName;
+        String author;
+        String wantCSS;
+        String wantJS;
+
+        System.out.print("Site name: ");
+        siteName = newScan.nextLine();
+        System.out.print("Author: ");
+        author = newScan.nextLine();
+        System.out.print("Do you want a folder for JavaScript? ");
+        wantJS = newScan.nextLine();
+        System.out.print("Do you want a folder for CSS? ");
+        wantCSS = newScan.nextLine();
+
+
+//Create a website generator
+        generateWebsite newWebsite = new generateWebsite();
+
+//Set the attributes for the website
+        newWebsite.siteName = siteName;
+        newWebsite.author = author;
+        newWebsite.path = "src/main/java/ex43/";
+
+//Create the website and the HTML file
+        System.out.println("Created " + newWebsite.createWebsite());
+        System.out.println("Created " + newWebsite.createHTMl());
+
+//Output
+
+//If user wants to create a JS and CSS folder
+        if(wantJS.equals("y"))
+            System.out.println("Created " + newWebsite.createJSFolder());
+
+        if(wantCSS.equals("y"))
+            System.out.println("Created " + newWebsite.createCSSFolder());
+
+    }
+
+}
+
