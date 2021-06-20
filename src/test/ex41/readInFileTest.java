ad42878badf2d0e9743d2966438cf20febd95b0c
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-20 18:27:24 -0400

    Add files via upload

---
+++ src/test/ex41/readInFileTest.java
@@ -0,0 +1,12 @@
+package ex41;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+
+public class readInFileTest extends TestCase {
+
+    public void testMyReader() {        File file = new File("src/main/exercise41_input.txt");
+
+    }
+}
---
+++ src/test/ex46/wordCountTest.java
@@ -0,0 +1,17 @@
+package ex46;
+
+import junit.framework.TestCase;
+
+public class wordCountTest extends TestCase {
+
+    public void testMain() {
+
+    }
+
+    public void testSearchCount() {
+        wordCountTest test = new wordCountTest();
+
+
+    }
+
+}
