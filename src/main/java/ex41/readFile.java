49821dd780bb926771250da7f99dd4dc0c40db4e
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-20 18:20:06 -0400

    Update readFile.java

--- src/main/java/ex41/readFile.java
+++ src/main/java/ex41/readFile.java
@@ -2,37 +2,42 @@ package ex41;
 
 
 
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class readFile {
 
-    public String getData(){
-        // Scan in data from file
-        try {
-            File myObj = new File("src/main/exercise41_input.txt");
-            Scanner myReader = new Scanner(myObj);
-            if (myReader.hasNextLine()) {
-                do {
-                    //convert file to String
-                    String getData = myReader.nextLine();
-                    //print out file data
+    public static void main(String[] args) throws Exception {
 
-                    System.out.println(getData);
+        String inputFile = "/Users/korinneramcharitar/Desktop/exercise41_input.txt";
+        String outputFile = "/Users/korinneramcharitar/Desktop/exercise41_output.txt";
 
-                } while (myReader.hasNextLine());
-            }
-            myReader.close();
-            //if File not found
-        } catch (FileNotFoundException e) {
-            //print error message
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        FileReader fileReader = new FileReader(inputFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String inputLine;
+        List<String> lineList = new ArrayList<String>();
+        while ((inputLine = bufferedReader.readLine()) != null) {
+            lineList.add(inputLine);
+        }
+        fileReader.close();
+
+        Collections.sort(lineList);
+
+        FileWriter fileWriter = new FileWriter(outputFile);
+        PrintWriter out = new PrintWriter(fileWriter);
+        for (String outputLine : lineList) {
+            out.println(outputLine);
+        }
+        out.flush();
+        out.close();
+        fileWriter.close();
 
     }
-
-        return getData();
-    }
-
 }
-
